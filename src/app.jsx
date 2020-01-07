3d256e1f67ad3a94fe5bb5caf72702e6d1fde993
srujansurapaneni <srujansurapaneni@gmail.com>
2020-01-07 01:24:32 -0500

    added initial code and first set of changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'></div>
+      <div className="conatiner">
+        <br />
+        <div className="alert alert-secondary" role="alert">
+          <h4>San Diego Top Spots</h4>
+          <h6>A list of the top 30 places to see in San Diego, California.</h6>
+        </div>
+      </div>
     );
   }
 }
